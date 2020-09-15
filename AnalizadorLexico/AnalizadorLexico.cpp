97228b0505109e0b70a2a624eaf2d140c97ed596
Mayra Cabrero <mayra-janeth42@hotmail.com>
2020-09-14 20:12:33 -0500

    operadores

--- AnalizadorLexico/AnalizadorLexico.cpp
+++ AnalizadorLexico/AnalizadorLexico.cpp
@@ -2,3 +2,4 @@
 /*^[0-9]+((\\.[0-9]+)|(\\.[0-9]+[E](\\+|\\-|[0-9]+)[0-9]+)|([E](\\+|\\-|[0-9]+)[0-9]+))$  esto es para flotantes*/
 /*^[a-z]+\d*$ esto es para variables*/
 /*^(auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while)$ para palabras reservadas*/
+/*^(\+|\-|%|\*|\/|=)$ esto es para operadores*/
