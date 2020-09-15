978b9ba165f4d20868ae85793d26fec84b05ea49
Mayra Cabrero <mayra-janeth42@hotmail.com>
2020-09-14 21:29:16 -0500

    Listo calisto

--- AnalizadorLexico/AnalizadorLexico.cpp
+++ AnalizadorLexico/AnalizadorLexico.cpp
@@ -11,60 +11,66 @@
 #include <string>
 
 using namespace std;
-string str;
-bool accepted=false;
 
-regex flotante("^[0-9]+((\\.[0-9]+)|(\\.[0-9]+[E](\\+|\\-|[0-9]+)[0-9]+)|([E](\\+|\\-|[0-9]+)[0-9]+))$");
-regex variables("^[a-z]+\d*$");
-regex reservadas("^(auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while)$");
-regex operadores("^(\\+|\\-|%|\\*|\\/|=)$");
-regex especiales("^(\\{|\\}|\\[|\\]|&&|\\|\\||\\;|\\,|\\.|\"\"|\" |\'|\'\'|<|>|<<|>>|\\^|<=|>=|==|\\||&)$");
-regex enteros("^\d+$");
-
-cout << "\t  A N A L I Z A D O R  L E X I C O" << endl << endl;
-while (true)
+int main() 
 {
-	cout << "Ingrese cadena: "; cin >> str; cout << " - ";
-	
-	accepted = regex_match(str, flotante);
-	if (accepted == true)
+	string str;
+	bool accepted = false;
+
+	regex flotante("^[0-9]+((\\.[0-9]+)|(\\.[0-9]+[E](\\+|\\-|[0-9]+)[0-9]+)|([E](\\+|\\-|[0-9]+)[0-9]+))$");
+	regex variables("^[a-z]+\\d*$");
+	regex reservadas("^(auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while)$");
+	regex operadores("^(\\+|\\-|%|\\*|\\/|=)$");
+	regex especiales("^(\\{|\\}|\\[|\\]|&&|\\|\\||\\;|\\,|\\.|\"\"|\" |\'|\'\'|<|>|<<|>>|\\^|<=|>=|==|\\||&)$");
+	regex enteros("^\\d+$");
+
+	cout << "\t  A N A L I Z A D O R  L E X I C O" << endl << endl;
+	while (true)
 	{
-		cout << "La cadena ingresada es un flotante" << endl;
+		cout << "Ingrese cadena: "; cin >> str; 
+
+		for (int i = 1; i <= 6; i++)
+		{
+			switch (i)
+			{
+			case 1: accepted = regex_match(str, flotante);
+				if (accepted == true)
+				{
+					cout << "La cadena ingresada es un flotante" << endl; break;
+				}
+			case 2: accepted = regex_match(str, variables);
+				if (accepted == true)
+				{
+					cout << "La cadena ingresada es una variable" << endl; break;
+				}
+			case 3: accepted = regex_match(str, reservadas);
+				if (accepted == true)
+				{
+					cout << "La cadena ingresada es una palabra reservada" << endl; break;
+				}
+			case 4: accepted = regex_match(str, operadores);
+				if (accepted == true)
+				{
+					cout << "La cadena ingresada es un operador" << endl; break;
+				}
+			case 5: accepted = regex_match(str, especiales);
+				if (accepted == true)
+				{
+					cout << "La cadena ingresada es un caracter especial" << endl; break;
+				}
+			case 6: accepted = regex_match(str, enteros);
+				if (accepted == true)
+				{
+					cout << "La cadena ingresada es un entero" << endl; break;
+				}
+			default:
+				cout << "La cadena ingresada es invalida" << endl; break;
+			} break;
+		}
+
+		accepted = false;
+		cout << endl;
 	}
 
-	accepted = regex_match(str, variables);
-	if (accepted == true)
-	{
-		cout << "La cadena ingresada es una variable" << endl;
-	}
-
-	accepted = regex_match(str, reservadas);
-	if (accepted == true)
-	{
-		cout << "La cadena ingresada es una palabra reservada" << endl;
-	}
-
-	accepted = regex_match(str, operadores);
-	if (accepted == true)
-	{
-		cout << "La cadena ingresada es un operador" << endl;
-	}
-
-	accepted = regex_match(str, especiales);
-	if (accepted == true)
-	{
-		cout << "La cadena ingresada es un caracter especial" << endl;
-	}
-
-	accepted = regex_match(str, enteros);
-	if (accepted == true)
-	{
-		cout << "La cadena ingresada es un entero" << endl;
-	}
-
-	if (accepted == false)
-	{
-		cout << "La cadena ingresada es invalida" << endl;
-	}
-	
+	return 0;
 }
