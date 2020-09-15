958fae69995f8f1658d4922733fbd4ea7cc0574c
Mayra Cabrero <mayra-janeth42@hotmail.com>
2020-09-14 20:28:18 -0500

    enteros y caracteres especiales

--- AnalizadorLexico/AnalizadorLexico.cpp
+++ AnalizadorLexico/AnalizadorLexico.cpp
@@ -3,3 +3,5 @@
 /*^[a-z]+\d*$ esto es para variables*/
 /*^(auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while)$ para palabras reservadas*/
 /*^(\+|\-|%|\*|\/|=)$ esto es para operadores*/
+/*^(\{|\}|\[|\]|&&|\|\||\;|\,|\.|""|"|'|''|<|>|<<|>>|\^|<=|>=|==|\||&)$ esto es para caracteres especiales*/
+/*^\d+$ esto es para enteros*/
